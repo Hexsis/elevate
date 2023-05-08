ef93096f3c158a55fe0baf762087b3c95073f5ad
Kazi Waseef <kazi.waseef@shopup.org>
2023-05-09 00:36:32 +0600

    feat(classes): added base classes

--- packages/elevate/lib/src/elevate_base.dart
+++ packages/elevate/lib/src/elevate_base.dart
@@ -1,6 +1,51 @@
-// TODO: Put public facing types in this file.
+import 'dart:async';
+import 'dart:io';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class Elevate {
+  final ElevateCoreModuleFeatures coreModule;
+  ElevateOptions? options;
+  Elevate({required this.coreModule, this.options});
+  Future<ElevateHttpServer> createHttpServer() async {
+    return ElevateHttpServer(coreModule);
+  }
 }
+
+class ElevateOptions {}
+
+abstract class ElevateCoreModuleFeatures {
+  Map<String, RequestHandler> get routes;
+
+  Future<String> handle(ElevateRequestContext context) async {
+    if (routes.containsKey(context.url)) {
+      return await routes[context.url]!.call(context);
+    }
+    return 'Handle Error';
+  }
+}
+
+class ElevateRequestContext {
+  final String url;
+
+  ElevateRequestContext({required this.url});
+}
+
+class ElevateHttpServer {
+  final ElevateCoreModuleFeatures coreModule;
+
+  ElevateHttpServer(this.coreModule);
+
+  Future<void> listen({int? port}) async {
+    port = port ?? 1337;
+    var server = await HttpServer.bind(InternetAddress.anyIPv6, port);
+    await server.forEach((HttpRequest request) async {
+      request.response.write(
+          await coreModule.handle(ElevateRequestContext(url: 'GET /my')));
+      request.response.close();
+    });
+  }
+}
+
+typedef RequestHandler = FutureOr<String> Function(
+    ElevateRequestContext context);
+
+
--- packages/elevate_module_generator/lib/src/elevate_module_generator_base.dart
+++ packages/elevate_module_generator/lib/src/elevate_module_generator_base.dart
@@ -1,6 +1,37 @@
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class ElevateCoreModule {
+  const ElevateCoreModule();
 }
+
+class ElevateModule {
+  const ElevateModule();
+}
+
+class Controller {
+  final String prefix;
+  const Controller({required this.prefix});
+}
+
+class Get {
+  final String? path;
+  const Get({this.path});
+}
+
+class Query {
+  final String? accessor;
+  const Query([this.accessor]);
+}
+
+class Param {
+  final String? accessor;
+  const Param([this.accessor]);
+}
+
+class Body {
+  final String? accessor;
+  const Body([this.accessor]);
+}
+
+class Service {
+  const Service();
+}
+
